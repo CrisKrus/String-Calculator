1541cf900ff61593de9ee95053d1716dac498e24
CrisKrus <cristian.suarez.ulpgc@gmail.com>
2018-02-14 12:19:01 +0000

    Red: test__given_one_number

--- src/TestCalculator.py
+++ src/TestCalculator.py
@@ -10,3 +10,6 @@ class TestCalculator(unittest.TestCase):
     def test__given_two_numbers_of_one_digit(self):
         self.assertEqual(2, add("1, 1"))
         self.assertEqual(10, add("6, 4"))
+
+    def test__given_one_number(self):
+        self.assertEqual(7, add("7"))
