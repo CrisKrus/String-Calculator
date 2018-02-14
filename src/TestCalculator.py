ef956927a085d4a7f5fd583588caea23c7055deb
CrisKrus <cristian.suarez.ulpgc@gmail.com>
2018-02-14 12:23:41 +0000

    Refactor: edit test__given_one_number name

--- src/TestCalculator.py
+++ src/TestCalculator.py
@@ -14,6 +14,6 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(2, add("1, 1"))
         self.assertEqual(10, add("6, 4"))
 
-    def test__given_one_number(self):
+    def test__given_one_number_of_one_digit(self):
         self.assertEqual(7, add("7"))
         self.assertEqual(4, add("4"))
