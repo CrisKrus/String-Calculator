38684d950542e6f3f25d5bbb9595ba8714ec1557
CrisKrus <cristian.suarez.ulpgc@gmail.com>
2018-02-14 12:25:23 +0000

    Red: test__given_empty_string

--- src/TestCalculator.py
+++ src/TestCalculator.py
@@ -17,3 +17,6 @@ class TestCalculator(unittest.TestCase):
     def test__given_one_number_of_one_digit(self):
         self.assertEqual(7, add("7"))
         self.assertEqual(4, add("4"))
+
+    def test__given_empty_string(self):
+        self.assertEqual(0, add(""))
